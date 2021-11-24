27cd449cf9815f44cfaec9f8506bd89b39df0f0c
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-11-24 17:29:15 +0100

    Initial project setup
    Mostly the default “Empty Activity” preset of Android Studio, but with
    view binding and some default resources.

---
+++ app/src/main/java/com/micha/evereview/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.micha.evereview
+
+import androidx.appcompat.app.AppCompatActivity
+import com.micha.evereview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val layout by lazy {ActivityMainBinding.inflate(layoutInflater)}
+}
