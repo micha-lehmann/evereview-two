fc6122c097c061b00bced126ad5b0020c780d041
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-11-29 16:32:55 +0100

    Add Hilt for dependency injection

---
+++ app/src/main/java/com/micha/evereview/Evereview.kt
@@ -0,0 +1,7 @@
+package com.micha.evereview
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Evereview : Application()
--- app/src/main/java/com/micha/evereview/main/MainActivity.kt
+++ app/src/main/java/com/micha/evereview/main/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.micha.evereview.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(), MainPresenter.View {
     private val layout by lazy {ActivityMainBinding.inflate(layoutInflater)}
     private val presenter = MainPresenter(this)
