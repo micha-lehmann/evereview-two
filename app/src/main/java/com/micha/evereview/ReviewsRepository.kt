5ce454a83a82d83da4024aa727c4357237fd5739
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-12-01 16:48:29 +0100

    Add repository

---
+++ app/src/main/java/com/micha/evereview/ReviewsRepository.kt
@@ -0,0 +1,9 @@
+package com.micha.evereview
+
+import android.util.Log
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ReviewsRepository @Inject constructor() {
+}
--- app/src/main/java/com/micha/evereview/main/MainPresenter.kt
+++ app/src/main/java/com/micha/evereview/main/MainPresenter.kt
@@ -1,11 +1,12 @@
 package com.micha.evereview.main
 
+import com.micha.evereview.ReviewsRepository
 import com.micha.evereview.models.Review
 import javax.inject.Inject
 
 class MainPresenter @Inject constructor(
     private val view: MainContract.View,
-) {
+    private val repo: ReviewsRepository
 ) : MainContract.Presenter {
     override val reviewsAdapter = ReviewsAdapter()
 
