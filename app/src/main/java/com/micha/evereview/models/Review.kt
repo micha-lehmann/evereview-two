a69c2d6c977c76a4b709fb458f548c370bfe72d8
Micha Lehmann <micha.lehmann@brickmakers.de>
2021-12-03 08:28:10 +0100

    Remove Review#time

--- app/src/main/java/com/micha/evereview/models/Review.kt
+++ app/src/main/java/com/micha/evereview/models/Review.kt
@@ -1,10 +1,7 @@
 package com.micha.evereview.models
 
-import java.util.*
-
 data class Review(
     val text: String,
-    val time: Date,
     val metadata: Map<String, Any?>,
     val itemTitle: String,
     val itemMetadata: Map<String, Any?>
